b0c80d972e090603eeed8f57479b2f1e2c02dd5b
20090392@alumno.utmetropolitana.edu.mx <20090392@alumno.utmetropolitana.edu.mx>
2021-10-04 12:20:55 -0500

    Agregar archivos de proyecto.

---
+++ Anagramas/Controllers/Anagrama.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagramas.Controllers
+{
+    /*
+     
+   Nombre de la escuela: Universidad Tecnologica Metropolitana
+
+
+
+    Asignatura: Aplicaciones Web Orientadas a Servicios
+
+
+
+
+    Nombre del Maestro: Chuc Uc Joel Ivan
+
+
+
+
+    Nombre de la actividad: Actividad 3 "Anagrama"
+
+
+
+
+    Nombre del alumno: Fabian Francisco Aguilar Rivero
+
+
+
+
+    Cuatrimestre: 4
+
+
+
+
+    Grupo: B
+
+
+
+
+    Parcial: 1
+    
+    */
+
+
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Anagrama : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Anagramas(string palabraoriginal, string palabranueva)
+        {
+            bool Anagrama;
+            var Resultado = "";
+
+            if (palabraoriginal.Length != palabranueva.Length)
+            {
+                Anagrama = false;
+            }
+
+            else
+            {
+                char[] A = palabraoriginal.ToCharArray();
+
+                Array.Sort(A);
+
+
+
+                char[] B = palabranueva.ToCharArray();
+
+                Array.Sort(B);
+
+
+
+                string PalabraO = new string(A);
+                string PalabraN = new string(B);
+                if (PalabraO == PalabraN)
+                {
+                    Anagrama = true;
+                }
+                else
+                {
+                    Anagrama = false;
+                }
+
+            }
+            if (Anagrama == true)
+            {
+                Resultado = "Las palabras " + palabraoriginal + " y " + palabranueva + " Son Anagramas";
+            }
+
+            else
+            {
+                Resultado = "Las palabras " + palabraoriginal + " y " + palabranueva + " No son Anagramas";
+            }
+
+
+            return Ok(Resultado);
+        }
+
+    }
+}
